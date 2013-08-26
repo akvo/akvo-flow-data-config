7a2453e5ac1c6c4fe942d1c518fc28121940429a
Iván Perdomo <ivan@akvo.org>
2013-08-26 14:02:25 +0200

    Fixes parameter calls

--- stats/helper.sh
+++ stats/helper.sh
@@ -3,7 +3,7 @@
 find "$1" -name 'appengine-web.xml' -exec grep '<application>' {} \; | sed 's/<[/]*application>//g' | sed 's/^[ \t]*//;s/[ \t]*$//' | sort > /tmp/instances.txt
 
 cd "$2"
-python stats.py "$1" "$2" /tmp/instances.txt
+python stats.py "$3" "$4" /tmp/instances.txt
 
 mkdir -p /tmp/akvo/flow/reports/stats
 mv /tmp/stats.txt /tmp/akvo/flow/reports/stats/$(date +"%Y-%m-%d").csv
